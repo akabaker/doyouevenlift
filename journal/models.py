3d62064ff5caf8f46e20305bd0c6010011950f9d
Luke Baker <akabaker@gmail.com>
2013-04-17 23:26:41 -0500

    Adding exercise, set model

--- journal/models.py
+++ journal/models.py
@@ -9,3 +9,19 @@ class Workout(models.Model):
 
     def get_absolute_url(self):
         return reverse('workouts-view', kwargs={'pk': self.id})
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=255)
+    workout = models.ForeignKey(Workout)
+
+    def __str__(self):
+        return self.name
+
+class Set(models.Model):
+    exercise = models.ForeignKey(Exercise)
+    reps = models.IntegerField(max_length=2)
+    max_reps = models.IntegerField(max_length=2)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.max_reps
